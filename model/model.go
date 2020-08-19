aa3f58c428773911361cc62b45efca4cf1f0f5d4
Muhammad Risky Negoro Putro <risky.putro@lemonilo.com>
2020-08-19 16:59:05 +0700

    set yml

--- model/model.go
+++ model/model.go
@@ -1,1 +1,11 @@
 package model
+
+type Config struct {
+	DBHost     string `yaml:"dbHost"`
+	DBPort     string `yaml:"dbPort"`
+	DBName     string `yaml:"dbName"`
+	DBUser     string `yaml:"dbUser"`
+	DBPassword string `yaml:"dbPassword"`
+	DBType     string `yaml:"dbType"`
+	TableName  string `yaml:"tableName"`
+}
