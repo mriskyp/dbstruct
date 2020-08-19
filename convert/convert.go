c8feae4c8e384278b3eed65196d9db682bf0bd6d
Muhammad Risky Negoro Putro <risky.putro@lemonilo.com>
2020-08-19 15:31:31 +0700

    set struct db

---
+++ convert/convert.go
@@ -0,0 +1,21 @@
+package converts
+
+import "unicode"
+
+// UpperInitial ...
+func UpperInitial(str string) string {
+	if len(str) == 0 {
+		return ""
+	}
+	tmp := []rune(str)
+	tmp[0] = unicode.ToUpper(tmp[0])
+	return string(tmp)
+}
+
+// LowerInitial ...
+func LowerInitial(str string) string {
+	for i, v := range str {
+		return string(unicode.ToLower(v)) + str[i+1:]
+	}
+	return ""
+}
